8ea16f3406e254182e44561078b1e3cf5a87c3b2
Red <red.sarouf2014@gmail.com>
2023-07-15 15:10:01 -0400

    add python badges and schedules lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,31 @@
 def badge_maker(name):
-    return None
+    print_message = "Hello, my name is " + name + "."
+    return print_message
 
 def batch_badge_creator(names):
-    return None
+    list_of_badges = []
+    for name in names:
+        
+        badge = badge_maker(name)
+        list_of_badges.append(badge)
+    return list_of_badges
 
 def assign_rooms(names):
-    return None
+    new_list = []
+    rooms = ["1", "2", "3", "4", "5", "6", "7", "8"]
+    for i in range(len(names)):
+        print(badge_maker(names[i]))
+        welcome_message_room_assignment = "Hello, " + names[i] + "! You'll be assigned to room " + rooms[i] + "!"   
+        new_list.append(welcome_message_room_assignment)
+       
+    return new_list
+    
 
 def printer(names):
-    return None
+
+    batch_badge_creator(names)
+    for room_assignment in assign_rooms(names):
+        print(room_assignment)
+
+
+printer(["Arel", "Carol"])
